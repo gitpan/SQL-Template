e674b8164ba7a9d391c447c049df62508756c44a
miguel prz <niceperl@gmail.com>
2014-04-20 08:13:17 +0000

    Import of NICEPERL/SQL-Template-0.2.0 from CPAN.
    gitpan-cpan-distribution: SQL-Template
    gitpan-cpan-version:      0.2.0
    gitpan-cpan-path:         NICEPERL/SQL-Template-0.2.0.tar.gz
    gitpan-cpan-author:       NICEPERL
    gitpan-cpan-maturity:     released

--- lib/SQL/Template.pm
+++ lib/SQL/Template.pm
@@ -13,11 +13,11 @@ SQL::Template - A new way to organize your database code
 
 =head1 VERSION
 
-Version 0.1.0
+Version 0.2.0
 
 =cut
 
-our $VERSION = '0.1.0';
+our $VERSION = '0.2.0';
 
 =head1 SYNOPSIS
 
